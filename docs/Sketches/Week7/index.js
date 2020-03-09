c2b5e450355b8adfcf6c9f01317aa929539b639d
Sam Hirsch <362328@colum.edu>
2020-03-09 15:18:12 -0500

    week 7 sketches

---
+++ docs/Sketches/Week7/index.js
@@ -0,0 +1,23 @@
+let n = 8;
+
+console.log("test");
+console.log(n);
+
+// Object. Similar to a struct or class
+let student1 = {name: "doug", age: 78, year: "senior", isSleeping: true}
+
+let myBox = document.querySelector(".box");
+
+myBox.style.width = "100px";
+myBox.style.height = "100px";
+myBox.innerHTML = "Box";
+
+function addNumbers()
+{
+    let n = n + 5;
+}
+
+let title = document.getElementById("Title");
+let button = document.getElementById("button");
+
+button.addEventListener('click', function(){});
