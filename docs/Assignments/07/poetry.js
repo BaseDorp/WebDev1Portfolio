344bb0be059018c3b3744d8d1bf64971429df61d
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-04-02 13:48:41 -0500

    finished assignment 7 (intro to javascript)

--- docs/Assignments/07/poetry.js
+++ docs/Assignments/07/poetry.js
@@ -1,6 +1,17 @@
-let poem = "";
+let lines = [];
 
-function addPoem(string)
+let poem = document.getElementById("input");
+let button = document.getElementById("enterButton");
+let inputField = document.getElementById("inputField");
+let poemResult = document.getElementById("poemResult");
+
+function AddLine()
 {
-    let poem = poem + " " + string;
-}
+    // Gets the string from the input field
+    let inputString = inputField.value;
+    // adds the string to the array of poem lines
+    lines.push(inputString);
+
+    // Adds the text to the string
+    poemResult.innerHTML += inputString + ("<br>");
+};
