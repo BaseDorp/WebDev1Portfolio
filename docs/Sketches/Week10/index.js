f7ca330728cd650d18516c849e17c59727d0db3b
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-04-06 14:36:00 -0500

    adds week 10 sketches and some changes to assignment 6

---
+++ docs/Sketches/Week10/index.js
@@ -0,0 +1,10 @@
+// JQuery
+$(document).ready(function()
+{
+    $("#title").css("color", "red");
+
+    // changes background color of all p elements
+    $("p").css("background-color", "blue");
+
+    $(".box").addClass("animated rubberBand");
+});
